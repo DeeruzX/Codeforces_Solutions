519adfb182e85fc0c89d880bb73617a36fd9f7b9
SleepKnight8 <jehadcoder@gmail.com>
2026-04-07 10:34:01 +0300

    Implement logic for 67th integer problem

---
+++ A_The_67th_Integer_Problem.py
@@ -0,0 +1,11 @@
+test_num = int(input())
+
+for _ in range(test_num):
+    main_input = int(input())
+    if test_num == 67:
+        print(67)
+    elif main_input == 67:
+        print(67)
+    else:
+        print(main_input+1)
+        
